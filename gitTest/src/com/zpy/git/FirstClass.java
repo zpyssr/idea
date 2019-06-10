3eced69b5dad9db2c43e6c382ee16ed94172d6e2
zpy <zpyssr@gmail.com>
2019-06-10 17:17:09 +0800

    差点删库

---
+++ gitTest/src/com/zpy/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.zpy.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+        System.out.println("hello,git project");
+
+    }
+}
---
+++ gitTest2/src/com/zpy/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.zpy.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("laix");
+        System.out.println("laix");
+        System.out.println("laix");
+        System.out.println("laix");
+        System.out.println("laix");
+        System.out.println("laix");
+    }
+}
